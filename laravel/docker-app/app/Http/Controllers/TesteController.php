c873bbd54cd6b705983ce8347b4a4ef826c43354
Bruno <brunorsantos1@gmail.com>
2018-12-10 20:37:36 -0200

    xdebug

---
+++ laravel/docker-app/app/Http/Controllers/TesteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TesteController extends Controller
+{
+    public function teste()
+    {
+        $a = 'i';
+        $b = '1';
+        return('teste');
+    }
+}
